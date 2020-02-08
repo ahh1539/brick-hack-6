df9394d9a193025d61f8cf78189bbef732ee34fc
ahh1539 <ahh1539@rit.edu>
2020-02-08 16:14:36 -0500

    basic layout done

---
+++ app/views.py
@@ -0,0 +1,12 @@
+from flask import render_template, url_for
+
+from app import app
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    return render_template("index.html")
+
+
+@app.route('/about', methods=['GET', 'POST'])
+def about():
+    return render_template("about.html")
--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/alexhurley/Desktop/myproject/venv"
+set -gx VIRTUAL_ENV "/Users/alexhurley/Desktop/brickhack6-project/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
