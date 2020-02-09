4f4dfa9cf843a6efb8936601e53efe7e4eba95aa
Dylan Gomer <dg1229@rit.edu>
2020-02-09 02:30:41 -0500

    Able to post listings; connected to MySQL database.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,8 @@
-from flask import render_template, url_for
-
+from flask import Flask,request,render_template, url_for
+from flaskext.mysql import MySQL
 from app import app
+
+
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
@@ -24,3 +26,22 @@ def signup():
 @app.route('/sell', methods=['GET', 'POST'])
 def sell():
     return render_template("sell.html")
+
+@app.route('/sell_item', methods=['GET', 'POST'])
+def sell_item():
+    app = Flask(__name__)
+    app.config['MYSQL_DATABASE_USER'] = 'root'
+    app.config['MYSQL_DATABASE_PASSWORD'] = 'Lads69'
+    app.config['MYSQL_DATABASE_HOST'] = '35.243.163.114'
+    app.config['MYSQL_DATABASE_DB'] = 'brick'
+    mysql = MySQL()
+    mysql.init_app(app)
+    Item_Name = request.form['Item_Name']
+    Price = request.form['Price']
+    Description = request.form['Description']
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    query = ("""INSERT INTO items (item_name,price,dets) VALUES(%s,%s,%s)""")
+    cursor.execute(query, (Item_Name,Price,Description))
+    conn.commit()
+    return render_template("created.html")
