0ab03aedb4f7f637a66d013b430b2e631dabed9f
ahh1539 <ahh1539@rit.edu>
2020-02-09 00:32:16 -0500

    ddgdfydbfs

--- app/views.py
+++ app/views.py
@@ -1,20 +1,26 @@
-from flask import render_template, url_for
+from flask import render_template, url_for, redirect, request
 
 from app import app
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/home', methods=['GET', 'POST'])
 def index():
     return render_template("index.html")
 
 
-@app.route('/about', methods=['GET', 'POST'])
+@app.route('/about')
 def about():
     return render_template("about.html")
 
 
-@app.route('/login', methods=['GET', 'POST'])
+@app.route('/', methods=['GET', 'POST'])
 def login():
-    return render_template("login.html")
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = "Invalid Credentials"
+        else:
+            return redirect(url_for('index'))
+    return render_template("login.html", error=error)
 
 
 @app.route('/signup', methods=['GET', 'POST'])
