8b252b58c0ab82d712902047b3497041c188a58f
ahh1539 <ahh1539@rit.edu>
2020-02-09 04:03:52 -0500

    sdnsdhds

--- app/views.py
+++ app/views.py
@@ -1,5 +1,5 @@
 from flask import Flask,request,render_template, url_for,redirect
-# from flaskext.mysql import MySQL
+from flaskext.mysql import MySQL
 from app import app
 
 
@@ -33,24 +33,24 @@ def signup():
 def sell():
     return render_template("sell.html")
 
-# @app.route('/sell_item', methods=['GET', 'POST'])
-# def sell_item():
-#     app = Flask(__name__)
-#     app.config['MYSQL_DATABASE_USER'] = 'root'
-#     app.config['MYSQL_DATABASE_PASSWORD'] = 'Lads69'
-#     app.config['MYSQL_DATABASE_HOST'] = '35.243.163.114'
-#     app.config['MYSQL_DATABASE_DB'] = 'brick'
-#     mysql = MySQL()
-#     mysql.init_app(app)
-#     Item_Name = request.form['Item_Name']
-#     Price = request.form['Price']
-#     Description = request.form['Description']
-#     conn = mysql.connect()
-#     cursor = conn.cursor()
-#     query = ("""INSERT INTO items (item_name,price,dets) VALUES(%s,%s,%s)""")
-#     cursor.execute(query, (Item_Name,Price,Description))
-#     conn.commit()
-#     return render_template("created.html")
+@app.route('/sell_item', methods=['GET', 'POST'])
+def sell_item():
+    app = Flask(__name__)
+    app.config['MYSQL_DATABASE_USER'] = 'root'
+    app.config['MYSQL_DATABASE_PASSWORD'] = 'Lads69'
+    app.config['MYSQL_DATABASE_HOST'] = '35.243.163.114'
+    app.config['MYSQL_DATABASE_DB'] = 'brick'
+    mysql = MySQL()
+    mysql.init_app(app)
+    Item_Name = request.form['Item_Name']
+    Price = request.form['Price']
+    Description = request.form['Description']
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    query = ("""INSERT INTO items (item_name,price,dets) VALUES(%s,%s,%s)""")
+    cursor.execute(query, (Item_Name,Price,Description))
+    conn.commit()
+    return render_template("created.html")
 
 @app.route('/expanded-card', methods=['GET', 'POST'])
 def expanded_card():
